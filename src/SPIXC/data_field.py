b535d9ca7c8b708d7b4c1c0999d09d21e998da2d
LauraneC <charriel@LJK-CVGI-147.local>
2026-04-17 14:27:16 +0200

    endable different sep in data field csv file

--- src/SPIXC/data_field.py
+++ src/SPIXC/data_field.py
@@ -35,7 +35,10 @@ class DataField:
 
     def _load_csv(self):
         data = pd.read_csv(self._filename, index_col=0, parse_dates=True)
-        data.index = pd.to_datetime(data.index)
+        try:data.index = pd.to_datetime(data.index)
+        except pd._libs.tslibs.parsing.DateParseError:
+            data = pd.read_csv(self._filename, index_col=0, parse_dates=True,sep=";")
+            data.index = pd.to_datetime(data.index)
         self._data = data
 
 ##COMPUTE
