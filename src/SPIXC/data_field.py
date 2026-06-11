de8c7e8a8fab3a058b800202a62a98ef72d0f01b
LauraneC <laurane.charrier@outlook.com>
2026-06-11 15:18:42 +0200

    add 1 sigma (68% of the distribution) in the metrics

--- src/SPIXC/data_field.py
+++ src/SPIXC/data_field.py
@@ -100,5 +100,8 @@ class DataField:
         evaluator = RegressionMetric(aligned["wse_2"].to_numpy(), aligned["wse_1"].to_numpy())
         results = evaluator.get_metrics_by_list_names(["RMSE", "MAE", "MAPE", "R2", "KGE","R"])
 
-        stats_tab = pd.DataFrame({"preprocessing":[preprocessing],"RMSE": [results["RMSE"]], "Bias": [bias], "KGE": [results["KGE"]],"MAE":[results["MAE"]], "MAPE":[results["MAPE"]], "R2": [results["R2"]],"R":[results["R"]],"nb_points":aligned.shape[0]})
+        absolute_errors = np.abs(aligned["wse_1"] - aligned["wse_2"])
+        percentile_68 = np.percentile(absolute_errors, 68)
+
+        stats_tab = pd.DataFrame({"preprocessing":[preprocessing],"RMSE": [results["RMSE"]], "Bias": [bias], "KGE": [results["KGE"]],"MAE":[results["MAE"]], "MAPE":[results["MAPE"]], "R2": [results["R2"]],"R":[results["R"]], "1 sigma":percentile_68,"nb_points":aligned.shape[0]})
         return stats_tab, aligned
